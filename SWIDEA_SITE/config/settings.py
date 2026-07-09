f1601d0fcab49153fee6124e89283f867628cec5
ethan0587 <leesch587@naver.com>
2026-07-09 15:07:32 +0900

    feat: docker 실습

--- SWIDEA_SITE/config/settings.py
+++ SWIDEA_SITE/config/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MYSQL_DATABASE", "swidea_db"),
+        "USER": os.environ.get("MYSQL_USER", "swidea_user"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "swidea_password"),
+        "HOST": os.environ.get("MYSQL_HOST", "swidea-db"),
+        "PORT": os.environ.get("MYSQL_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
